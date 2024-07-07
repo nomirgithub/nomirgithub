cb32c6e3a9178d3f36184b859fb858685bc056b9
nomirgithub <lailanomir3@gmail.com>
2024-07-07 21:54:47 +0300

    add my files

---
+++ projects/Did you know/script.js
@@ -0,0 +1,11 @@
+const articleBody = document.body;
+function darkMode() {
+    articleBody.style.backgroundColor = "black";
+    articleBody.style.color = "white";
+
+}
+function lightMode() {
+    articleBody.style.backgroundColor = "white";
+    articleBody.style.color = "black";
+
+}
